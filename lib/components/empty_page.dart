050300d5397df41cdf6b881cc790f3a95a895e11
nguyentea <81692943+nguyentea@users.noreply.github.com>
2023-04-01 23:07:43 +0700

    sua time

--- lib/components/empty_page.dart
+++ lib/components/empty_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -8,6 +9,8 @@ import 'package:fluttercontrolpanel/firebase_options.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart';
 class EmptyPage extends StatefulWidget {
   const EmptyPage({Key? key}) : super(key: key);
 
@@ -20,127 +23,124 @@ class _EmptyPageState extends State<EmptyPage> {
   Map<int, double> downloadProgress = {};
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    futureFiles = FirebaseStorage.instance.ref('').listAll();
+    futureFiles = FirebaseStorage.instance.ref('Video').listAll();
     //futureFiles = Firebase.initializeApp.ref('/file').listAll();
   }
-  @override
-  Widget build(BuildContext context) {
-    double? _progress;
-    return Scaffold(
-      body: FutureBuilder<ListResult>(
-        future: futureFiles,
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            final files = snapshot.data!.items;
-            return ListView.builder(
-              itemCount: files.length,
-              itemBuilder: (context, index){
-                final file = files[index];
-                double? progress = downloadProgress[index];
-
-                return ListTile(
-                  title: Text(file.name),
-                  subtitle: progress != null
-                    ?LinearProgressIndicator(
-                    value: progress,
-                    backgroundColor: Colors.black26,
-                  )
-                  :null,
-                  trailing: IconButton(
-                    icon: const Icon(
-                      Icons.download,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      // downloadFile(index, file);
-                      FileDownloader.downloadFile(url: file.getDownloadURL().toString(),
-                      onProgress:(name,progress){
-                        setState(() {
-                          _progress=progress;
-                        });
-                      },
-                      onDownloadCompleted: (value){
-                        print('complted');
-                      });
-                    },
-                  ),
-                );
-              },
-
-            );
-
-          }else if(snapshot.hasError){
-            return const Center(child: Text('Error occurred'),);
-          }else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
-
-    /*Future downloadFile(int index,Reference ref) async{
+  Future downloadFile(int index, Reference ref) async {
     final url = await ref.getDownloadURL();
 
+    final Directory directory = await getApplicationDocumentsDirectory();
     final tempDir = await getTemporaryDirectory();
-    final path = '${tempDir.path}/${ref.name}';
-    await Dio().download(
-        url,
-        path,
-        onReceiveProgress: (receied, total){
-          double progress = receied / total;
 
-          setState(() {
-            downloadProgress[index] = progress;
-          });
+    final path = '${tempDir.path}/${ref.name}';
+    // In ra đường dẫn của video để kiểm tra
+    print('Video downloaded to $path');
+
+
+    await Dio().download(
+      url,
+      path,
+      onReceiveProgress: (receied, total) {
+        double progress = receied / total;
+        setState(() {
+          downloadProgress[index] = progress;
+        });
       },
     );
 
-    if(url.contains('.mp4')){
+    if (url.contains('.mp4')) {
       await GallerySaver.saveVideo(path, toDcim: true);
-    } else if(url.contains('ipg')){
+    } else if (url.contains('ipg')) {
       await GallerySaver.saveImage(path, toDcim: true);
     }
 
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text('Downloaded ${ref.name}')),
     );
-  }*/
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<ListResult>(
+        future: futureFiles,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final files = snapshot.data!.items;
+            return ListView.builder(
+              itemCount: files.length,
+              itemBuilder: (context, index) {
+                final file = files[index];
+                double? progress = downloadProgress[index];
 
+
+                return ListTile(
+                  title: GetUserName(file.name),
+                  subtitle: progress != null
+                      ? LinearProgressIndicator(
+                    value: progress,
+                    backgroundColor: Colors.black26,
+                  )
+                      : null,
+                  trailing: IconButton(
+                    icon: const Icon(
+                      Icons.download,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {downloadFile(index,file);},
+                  ),
+                );
+              },
+
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error occurred'),);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
 
   }
-
-  // Future downloadFile(int index,Reference ref) async{
-  //   final url = await ref.getDownloadURL();
-
-  //   final Directory directory = await getApplicationDocumentsDirectory();
-  //   final tempDir = await getTemporaryDirectory();
-  //
-  //   final path = '${tempDir.path}/${ref.name}';
-  //   // In ra đường dẫn của video để kiểm tra
-  //   print('Video downloaded to $path');
-  //  await Dio().download(
-  //     url,
-  //     path,
-  //     onReceiveProgress: (receied, total){
-  //       double progress = receied / total;
-  //       setState(() {
-  //         downloadProgress[index] = progress;
-  //       });
-  //     },
-  //   );
-  //
-  //   if(url.contains('.mp4')){
-  //     await GallerySaver.saveVideo(path,toDcim: false);
-  //   } else if(url.contains('ipg')){
-  //     await GallerySaver.saveImage(path, toDcim: true);
-  //   }
-  //
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     SnackBar(content: Text('Downloaded ${ref.name}')),
-  //   );
-  // }
 }
+class GetUserName extends StatelessWidget {
+  final String documentId;
 
+
+  GetUserName(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('violation');
+    var Id = documentId.split('.');
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(Id[0]).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+        if (snapshot.hasError) {
+
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+          
+          final String preConverted = data['time'];
+
+          DateTime date = DateTime.parse(snapshot.data!['time'].toDate().toString());
+          print("ngay thang ${DateTime.fromMicrosecondsSinceEpoch(snapshot.data!["time"].microsecondsSinceEpoch)}");
+          return Text("${snapshot.data!['time'].toDate()}.${data['violation']}.${data['licenseplate']}.${data['local']}");
+
+
+        }
+        return Text("loading");
+
+      },
+
+    );
+  }
+}
